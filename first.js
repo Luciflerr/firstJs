97658f141959f6aacd6343ae7eeffa9c352ee640
Eray <129649763+Luciflerr@users.noreply.github.com>
2024-02-02 17:46:15 +0300

    Update first.js

--- first.js
+++ first.js
@@ -1,2 +1,11 @@
 console.log('Hello, its my first javascript code');
 console.log('YEEEEEYYYY!!!!');
+
+let hafta = "pazartesi";
+
+if (hafta === "pazartesi") {
+    console.log("Pazartesiden nefret ediyorum!!!")
+}
+else if (hafta === "cumartesi") {
+    console.log("Hafta sonunu seviyorummm!!!!!")
+}
